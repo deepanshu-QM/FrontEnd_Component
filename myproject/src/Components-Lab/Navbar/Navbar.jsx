4e961ec6742838f0bd8ce0a716bfc2ce7b43a6ff
deepanshu-QM <cdeepanshu231@gmail.com>
2026-02-12 23:48:42 +0530

    File struture Changes

---
+++ myproject/src/Components-Lab/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from './react'
+import './Navbar.css'
+
+const navbar = () => {
+    return (
+        <nav className='container'>
+            <img src={} alt='' className='logo'/>
+            <ul>
+                 <li>SHOP</li>
+                 <li>NEW IN</li>
+                 <li>ACCESSORIES</li>
+                 <li>ARCHIVES</li>
+                 <li>LAST CHANCE</li>
+                 <li>BEST SELLER</li>
+                 <li>KIDS</li>
+                 <li>MEN</li>
+                 <li>WOMEN</li>
+                 <li>HOME</li>
+                 <li>ABOUT</li>
+                 <li>HELP</li>
+            </ul>
+        </nav>
+    )
+}
+export default navbar;
