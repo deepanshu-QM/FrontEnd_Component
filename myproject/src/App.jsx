0fd44f93ab030c7522f20a3aff98aee78d844b03
deepanshu-QM <cdeepanshu231@gmail.com>
2026-02-12 23:16:11 +0530

    Changes

--- myproject/src/App.jsx
+++ myproject/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Button from './Components-Lab/Button/Button'
+import Input from './Components-Lab/Input/Input'
 function App(){
     return(
         <div>
-            <Button/>
+            
+            <Input/>
         </div>
     )
 }
 export default App
+
