1ad579024bd7ea232bbbe7794803a3bc9daa5af2
deepanshu-QM <cdeepanshu231@gmail.com>
2026-02-16 22:35:55 +0530

    Drop-Box

---
+++ README.md
@@ -0,0 +1,21 @@
+# 👕 Clothing Website
+
+# A modern full-stack clothing e-commerce web application built using the MERN stack. The platform provides a seamless shopping experience with intelligent features like chatbot assistance and personalized recommendations.
+
+🚀 Tech Stack
+🖥️ Frontend
+React.js – Dynamic and responsive user interface
+
+⚙️ Backend
+Node.js – Server-side runtime environment
+Express.js – API handling and routing
+
+🗄️ Database
+MongoDB – NoSQL database for storing user and product data
+
+✨ Key Features
+🛍️ Product browsing and filtering
+🔐 User authentication & authorization
+🛒 Shopping cart functionality
+💬 AI-powered Chatbot for handling customer queries
+🎯 Personalized Recommendation System based on
---
+++ myproject/src/Components-Lab/Menudropdown/Menudropdown.jsx
@@ -0,0 +1,51 @@
+
+import React from 'react'
+import './Menudropdown.css'
+
+const Menu = () => {
+    return(
+        <div className='Menucontainer'>
+            <div className='left'>
+
+                <div className='list-1'>
+                    <ul>
+                        <li><b>READY-TO-WEAR</b></li>
+                        <li>kNITWEAR</li>
+                        <li>TOPS</li>
+                        <li>JACKETS</li>
+                        <li>COATS</li>
+                        <li>DRESSES</li>
+                        <li>T-SHIRTS & SWEATSHIRTS</li>
+                        <li>PANTS</li>
+                        <li>DENIM</li>
+                        <li>SKIRTS & SHORTS</li>
+                        <li>BAGS</li>
+                        <li>SHOES</li>
+                        <li>JEWELRY</li>
+                        <li>BELTS</li>
+                        <li>SCARVES & BEANIES</li>
+                        <li>TIGHTS & SOCKS</li>
+                        <li>LIFESTYLE</li>
+                    </ul>
+                </div>
+
+                <div className='list-2'>
+                     <ul>
+                        <li><b>DISCOVER</b></li>
+                        <li>GIFT CARDS</li>
+                        <li>OUR SOLIDARITY</li>
+                        <li>PERSONALIZATION</li>
+                        <li>CITY GUIDE</li>
+                        <li>LAST CHANCE</li>
+                        <li>ARCHIVES</li>
+                        <li>L'EAU ZELOR</li>
+                    </ul>
+                </div>
+
+            </div>
+            <div className='right'>
+
+            </div>
+        </div>
+    )
+}
